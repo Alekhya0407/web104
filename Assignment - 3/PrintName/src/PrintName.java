8497e1301280eed821b63444f83021a1a72c2d28
Akhila1978 <85426511+Akhila1978@users.noreply.github.com>
2021-06-06 11:51:46 +0530

    Add files via upload

---
+++ Assignment - 3/PrintName/src/PrintName.java
@@ -0,0 +1,8 @@
+public class PrintName { 
+	public static void main(String[] args) { 
+		// for loop that prints "a name" ten times.  
+		for (int i = 0; i < 10; i++) { 
+			System.out.println("Bright IT Career "); 
+		} 
+	} 
+} 
