ac5fbc593878fd647b724343744b9e3ce0b84934
Akhila1978 <85426511+Akhila1978@users.noreply.github.com>
2021-06-06 11:48:31 +0530

    Delete Assignment - 3 directory

--- Assignment - 3/PrintName/src/PrintName.java
+++
@@ -1,8 +0,0 @@
-public class PrintName { 
-	public static void main(String[] args) { 
-		// for loop that prints "a name" ten times.  
-		for (int i = 0; i < 10; i++) { 
-			System.out.println("Bright IT Career "); 
-		} 
-	} 
-} 
