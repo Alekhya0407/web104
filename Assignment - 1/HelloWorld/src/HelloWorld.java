ed13637903a18d7ce6b840a10838f653b57a4529
Akhila1978 <85426511+Akhila1978@users.noreply.github.com>
2021-06-06 11:28:43 +0530

    Add files via upload

---
+++ Assignment - 1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+//my first comment
+public class HelloWorld{
+    //my second comment
+    public static void main(String args[]){
+        /** my third comment **/
+        System.out.println("hello world");
+    }
+}
---
+++ Assignment - 1/OverloadMain/src/OverloadMain.java
@@ -0,0 +1,12 @@
+class OverloadMain  
+{  
+public static void main(String xyz)  //overloaded main method  
+{  
+System.out.println(xyz);  
+}  
+public static void main(String args[])  
+{     
+System.out.println("main method incoked");  
+main("6");  
+} 
+}
